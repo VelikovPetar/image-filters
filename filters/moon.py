39890ceeec2ee747207055e41d6eb231d5193098
VelikovPetar <petarvelikov95@gmail.com>
2019-01-15 22:09:29 +0100

    Improve moon filter.

--- filters/moon.py
+++ filters/moon.py
@@ -1,20 +1,45 @@
 import cv2
 
+from filters.gingham import GinghamFilter
 from utils import image_utils
+import numpy as np
 
 
 class MoonFilter:
     # The points defining the tone curve
     TONE_CURVE_ANCHORS = [
-        (0, 0),
-        (22, 42),
-        (56, 60),
-        (91, 120),
-        (200, 219),
-        (246, 255)
+        (20, 52),
+        (30, 54),
+        (40, 67),
+        (50, 74),
+        (60, 82),
+        (70, 94),
+        (80, 122),
+        (90, 148),
+        (100, 161),
+        (110, 179),
+        (120, 186),
+        (130, 197),
+        (140, 198),
+        (150, 203),
+        (160, 208),
+        (170, 216),
+        (180, 223),
+        (190, 226),
+        (200, 231),
+        (210, 235),
+        (220, 243),
+        (230, 243),
+        (240, 246),
+        (250, 249),
+        (255, 255)
     ]
 
     def apply(self, source_img):
         image = cv2.cvtColor(source_img, cv2.COLOR_BGR2GRAY)
-        image = image_utils.map_pixel_values(image, image_utils.create_tone_curve(self.TONE_CURVE_ANCHORS))
+        image = image_utils.map_pixel_values(image, image_utils.create_tone_curve(self.TONE_CURVE_ANCHORS, True))
+        alpha = 1
+        beta = 0
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        image = image_utils.change_contrast_and_brightness(image, alpha, beta)
         return image
--- main.py
+++ main.py
@@ -21,13 +21,13 @@ FILTERS = {
 if __name__ == '__main__':
     img_name = 'res/image-me.jpg'
     image = image_utils.read_image(img_name)
-    filter_name = "gingham"
+    filter_name = "moon"
     filtered_image = FILTERS[filter_name].apply(image)
-    cv2.imwrite('res/gingham.jpg', filtered_image)
+    cv2.imwrite('res/moon.jpg', filtered_image)
 
     # Convert images from BGR to RGB so 'pyplot' can display them correctly
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    filtered_image = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2RGB)
-    plt.subplot(121), plt.imshow(image, 'gray'), plt.axis("off")
-    plt.subplot(122), plt.imshow(filtered_image, 'gray'), plt.axis("off")
-    plt.show()
+    # filtered_image = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2RGB)
+    # plt.subplot(121), plt.imshow(image, 'gray'), plt.axis("off")
+    # plt.subplot(122), plt.imshow(filtered_image, 'gray'), plt.axis("off")
+    # plt.show()
